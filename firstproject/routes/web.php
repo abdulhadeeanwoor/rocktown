c5d9f8e6e3bdb4f2d65f63d31cf9f5c690eac254
abdulhadeeanwoor <57297752+abdulhadeeanwoor@users.noreply.github.com>
2019-12-22 23:00:20 +0500

    Add files via upload

--- firstproject/routes/web.php
+++ firstproject/routes/web.php
@@ -11,19 +11,72 @@
 |
 */
 
-Route::get('welcome', function () {
-    return view('welcome');
-});
-
-Route::get('home', function () {
-    return view('home');
-});
-Route::get('contact', function () {
-    return view('contact');
-});
-
-Route::get('login', function () {
-    return view('login');
-});
 
 
+
+
+/*
+|--------------------------------------------------------------------------
+| Danial Changes
+|--------------------------------------------------------------------------
+|
+
+Route::get('/index', function () {
+    return view('js.index');
+});
+*/
+
+
+
+Route::get('/dashboard', 'load_images@getImages')->middleware('auth');
+
+
+
+/*
+|--------------------------------------------------------------------------
+| Danial Changes END
+|--------------------------------------------------------------------------
+*/
+
+
+
+
+
+
+
+// Route::get('/dashboard', function () {
+//     return view('dasboard');
+// });
+
+Route::resource('products', 'ProductController'); 
+Route::get('/getImages', 'load_images@getImages');
+
+
+Route::get('/', function () {
+    return view('auth.login');
+});
+
+Route::get('/','load_images@getImages');
+Route::get('/index','load_images@getImages');
+Route::get('/about','PagesController@getabout');
+Route::get('/blog','PagesController@getblog');
+Route::get('/cart','getcartdata@getcartitems');
+Route::get('/checkout','PagesController@getcheckout');
+Route::get('/contact','PagesController@getcontact');
+Route::get('/shop','PagesController@getshop');
+
+//Route::get('/index', 'load_images@getImages');
+//Route::get('/delete/{id}', 'load_images@delete');
+Route::POST('/insert', 'cartController@addtocart');
+Route::POST('/addtocart','cartController@addtocart');
+
+Route::get('/add/products','PagesController@createProduct');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+
+Route::get('/insertdata', function () {
+    return view('insert');
+});
